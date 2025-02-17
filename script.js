e738064af8a45d851ed9300aca39a65b2a236f41
Shujaat Azim <shujaat.azim@gmail.com>
2025-02-17 17:00:20 -0500

    Add descriptions in a mouseover for primary, secondary, and tertiary colors.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const tooltip = document.getElementById("tooltip");
+const legendDescription = document.getElementById("legend-description")
 
 document.querySelectorAll(".marker").forEach(marker => {
   marker.addEventListener("mouseenter", (event) => {
@@ -23,3 +24,34 @@ document.querySelectorAll(".marker").forEach(marker => {
     tooltip.style.opacity = "0";
   });
 });
+
+document.querySelectorAll(".legend").forEach(legend => {
+  legend.addEventListener("mouseenter", (event) => {
+    const legendRank = event.target.getAttribute("rank")
+    let legendContent = 'hello'
+    switch(legendRank) {
+      case "primary":
+        legendContent = "Primary colors are the fundamental building blocks for deriving all other colors on a digital screen.";
+        break;
+      case "secondary":
+        legendContent = "Secondary colors are made by combining two primary colors together in equal parts.";
+        break;
+      case "tertiary":
+        legendContent = "Tertiary colors are made by combining a primary color with a secondary color.";
+        break;
+      default:
+        legendContent = '';
+    }
+    legendDescription.textContent = legendContent;
+    legendDescription.style.opacity = "1";
+  })
+
+  legend.addEventListener("mousemove", (event) => {
+    legendDescription.style.left = event.pageX + 10 + "px";
+    legendDescription.style.top = event.pageY + 10 + "px";
+  });
+
+  legend.addEventListener("mouseleave", () => {
+    legendDescription.style.opacity = "0";
+  });
+})
